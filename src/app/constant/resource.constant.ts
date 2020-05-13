edb776e2782d71e1c96a6aebbf274b594d8d41dc
ravihd123 <ravihd74@gmail.com>
2020-05-13 17:00:23 +0530

    added resource file

---
+++ src/app/constant/resource.constant.ts
@@ -0,0 +1,13 @@
+export class ResourceConstants {
+
+    static RESOURCES = {
+        'login.username.label': 'USERNAME',
+        'login.password.label': 'PASSWORD',
+        'login.error.label': 'This is required field',
+        'login.submit.label': 'LOGIN',
+        'login.clear.label' : 'CLEAR',
+        'login.invalidCred.label' : ' username or password is incorrect',
+        
+    }
+
+};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { LoginService } from '../services/login.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 import { AuthGaurdService } from '../services/auth-guard.service';
+import { ResourceService } from '../services/resource/resource.service';
+import { LoginUiConfig } from './login-ui-config';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +15,35 @@ import { AuthGaurdService } from '../services/auth-guard.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   validLogin = true;
+  loginUiConfig: LoginUiConfig = {};
 
   constructor(
     private formBuilder: FormBuilder,
     private loginservice: LoginService,
     private route: Router,
-    private authGaurd: AuthGaurdService
+    private authGaurd: AuthGaurdService,
+    private resourceConstant: ResourceService
   ) { }
 
   ngOnInit(): void {
+    this.initResources()
     this.authGaurd.isLoggedOut()
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  private initResources() {
+    this.loginUiConfig = {
+      username: this.resourceConstant.getConstValue('login.username.label'),
+      password: this.resourceConstant.getConstValue('login.password.label'),
+      errorLabel: this.resourceConstant.getConstValue('login.error.label'),
+      submit: this.resourceConstant.getConstValue('login.submit.label'),
+      clearLabel: this.resourceConstant.getConstValue('login.clear.label'),
+      invalidCred: this.resourceConstant.getConstValue('login.invalidCred.label')
+    }
+
   }
 
   onSubmit() {
@@ -36,7 +53,6 @@ export class LoginComponent implements OnInit {
       this.route.navigateByUrl('dashboard')
     } else {
       // this.route.navigateByUrl('login')
-
     }
 
   }
